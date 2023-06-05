9efda6cd72bf9e4c07cc03572d0d1af2ff8d42d7
HyunggiPark <phg9587@naver.com>
2023-06-06 01:50:30 +0900

    API 테스트로 전환하기

---
+++ src/test/java/com/example/productorderservice/product/ProductApiTest.java
@@ -0,0 +1,41 @@
+package com.example.productorderservice.product;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ProductApiTest extends ApiTest {
+
+    private static AddProductRequest 상품등록요청_생성() {
+        final String name = "상품명";
+        final int price = 1000;
+        final DiscountPolicy discountPolicy = DiscountPolicy.NONE;
+        final AddProductRequest request = new AddProductRequest(name, price, discountPolicy);
+        return request;
+    }
+
+    private static ExtractableResponse<Response> 상품등록요청(AddProductRequest request) {
+        return RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post("/products")
+                .then()
+                .log().all().extract();
+    }
+
+    @Test
+    void 상품등록() {
+        final var request = 상품등록요청_생성();
+
+        //API 요청
+        final var response = 상품등록요청(request);
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+}
