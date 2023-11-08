b764894c7e15eb14bfb97c4509872a932200bfb4
HyunggiPark <phg9587@naver.com>
2023-11-08 20:03:14 +0900

    JPA 적용하기

--- src/main/java/com/example/productorderservice/payment/Payment.java
+++ src/main/java/com/example/productorderservice/payment/Payment.java
@@ -1,12 +1,23 @@
 package com.example.productorderservice.payment;
 
 import com.example.productorderservice.order.Order;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 class Payment {
-    private final Order order;
-    private final String cardNumber;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @OneToOne
+    private Order order;
+    private String cardNumber;
 
     public Payment(final Order order, final String cardNumber) {
         Assert.notNull(order, "order must not be null");
@@ -16,19 +27,8 @@ class Payment {
         this.cardNumber = cardNumber;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void assignId(final Long id) {
-        this.id = id;
-    }
-
     public int getPrice() {
         return order.getTotalPrice();
     }
 
-    public String getCardNumber() {
-        return cardNumber;
-    }
 }
--- src/main/java/com/example/productorderservice/payment/PaymentAdapter.java
+++ src/main/java/com/example/productorderservice/payment/PaymentAdapter.java
@@ -1,23 +1,25 @@
 package com.example.productorderservice.payment;
 
 import com.example.productorderservice.order.Order;
-import com.example.productorderservice.product.DiscountPolicy;
-import com.example.productorderservice.product.Product;
+import com.example.productorderservice.order.OrderRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 class PaymentAdapter implements PaymentPort {
     private final PaymentGateway paymentGateway;
     private final PaymentRepository paymentRepository;
+    private final OrderRepository orderRepository;
 
-    PaymentAdapter(final PaymentGateway paymentGateway, final PaymentRepository paymentRepository) {
+    PaymentAdapter(final PaymentGateway paymentGateway, final PaymentRepository paymentRepository, final OrderRepository orderRepository) {
         this.paymentGateway = paymentGateway;
         this.paymentRepository = paymentRepository;
+        this.orderRepository = orderRepository;
     }
 
     @Override
     public Order getOrder(final Long orderId) {
-        return new Order(new Product("상품1", 1000, DiscountPolicy.NONE), 2);
+        return orderRepository.findById(orderId)
+                .orElseThrow(() -> new IllegalArgumentException("order not found"));
     }
 
     @Override
